f148166e0d14a359d262e5189d76820fd07cf961
mukeshkumarjha7 <mukeshnitn@gmail.com>
2026-04-23 00:19:04 +0530

    Add Employee model scops Failures test

--- spec/models/employee_spec.rb
+++ spec/models/employee_spec.rb
@@ -53,4 +53,37 @@ RSpec.describe Employee, type: :model do
     new_record = Employee.new(subject.attributes)
     expect(new_record).not_to be_valid
   end
+#
+  it "return employee for given name" do
+    employee = Employee.create!(subject.attributes)
+    record = Employee.find_by_name("Mukesh Jha")
+    expect(record).to include(employee)
+  end
+
+  it "return empty if employee doesn't exist" do
+    record = Employee.by_name("Invalid User")
+    expect(record).to be_empty
+  end
+
+  it "return employee list for given country" do
+    employee = Employee.create!(subject.attributes)
+    record = Employee.by_country("India")
+    expect(record).to include(employee)
+  end
+
+  it "return empty if no employee belongs to given country" do
+    record = Employee.by_country("US")
+    expect(record).to be_empty
+  end
+
+  it "return employee list of given title" do
+    employee = Employee.create!(subject.attributes)
+    record = Employee.by_job_title("Software Engineer")
+    expect(record).to include(employee)
+  end
+
+  it "return empty if no employee has the given job title" do
+    record = Employee.by_job_title("HR")
+    expect(record).to be_empty
+  end
 end  
