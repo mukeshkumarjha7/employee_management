15b366d6268558851150f41c0f0f7cd0624a249e
mukeshkumarjha7 <mukeshnitn@gmail.com>
2026-04-23 01:20:10 +0530

    Resolve test failure for search endpoint in employee controller.

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -32,6 +32,25 @@ class EmployeesController < ApplicationController
     head :no_content
   end
 
+  def search
+    if params[:name].blank? && params[:country].blank? && params[:job_title].blank?
+      return render json: { error: "At least one search parameter (name, country, job_title) is required" }, status: :bad_request
+    end
+
+    employees = nil
+    if params[:name].present?
+      employees = Employee.by_name(params[:name])
+    elsif params[:country].present?
+      employees = Employee.by_country(params[:country])
+    elsif params[:job_title].present?
+      employees = Employee.by_job_title(params[:job_title])
+    else
+      render json: { errors: "Search parameter is empty" }, status: :bad_request
+    end
+
+    render json: employees, status: :ok
+  end
+
   private
 
   def set_employee
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
 
-  resources :employees
+  resources :employees do
+    collection do
+      get :search
+    end
+  end
 
   get "up" => "rails/health#show", as: :rails_health_check
 
