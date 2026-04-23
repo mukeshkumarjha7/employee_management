bf6f782031350630b8c0e152d6482e7da096daaa
mukeshkumarjha7 <mukeshnitn@gmail.com>
2026-04-23 12:02:41 +0530

    Resolve test failures for Employee views

--- spec/views/employees/index.html.erb_spec.rb
+++ spec/views/employees/index.html.erb_spec.rb
@@ -14,8 +14,22 @@ RSpec.describe "employees/index", type: :view do
 
   before(:each) do
     assign(:employees, [
-      Employee.create!(valid_attributes),
-      Employee.create!(valid_attributes.merge(email: "rahul@example.com", first_name: "Rahul"))
+      Employee.create!(
+        first_name: "Mukesh",
+        last_name: "Jha",
+        job_title: "Software Engineer",
+        country: "India",
+        salary: 50000,
+        email: "mukesh@example.com"
+      ),
+      Employee.create!(
+        first_name: "Rahul",
+        last_name: "Singh",
+        job_title: "Software Engineer",
+        country: "India",
+        salary: 65000,
+        email: "rahul@gmail.com"
+      )
     ])
   end
 
@@ -24,9 +38,9 @@ RSpec.describe "employees/index", type: :view do
     cell_selector = "div>p"
     assert_select cell_selector, text: Regexp.new("Mukesh"), count: 1
     assert_select cell_selector, text: Regexp.new("Rahul"),  count: 1
-    assert_select cell_selector, text: Regexp.new("Jha"),    count: 2
+    assert_select cell_selector, text: Regexp.new("Jha"),    count: 1
     assert_select cell_selector, text: Regexp.new("Software Engineer"), count: 2
     assert_select cell_selector, text: Regexp.new("India"),  count: 2
-    assert_select cell_selector, text: Regexp.new("50000"),  count: 2
+    assert_select cell_selector, text: Regexp.new("50000"),  count: 1
   end
 end
